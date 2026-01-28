d49fb20b9651bbf082c565886c1df67a8231400b
Goose test <roganyanegor@yandex.ru>
2026-01-28 22:23:21 +0300

    Исправил части с exclude и marks

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,39 +1,38 @@
 function Student(name, gender, age) {
-  this.name = name;
-  this.gender = gender;
-  this.age = age;
-  this.marks = [];
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  this.subject = subjectName;
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
+Student.prototype.addMarks = function(...marks) {
+	if (!Array.isArray(this.marks)) {
+		return;
+	}
 
-  if (!Array.isArray(this.marks)) {
-    return;
-  }
+	this.marks.push(...marks);
+};
 
-  this.marks.push(...marks);
+Student.prototype.getAverage = function() {
+
+	if (!Array.isArray(this.marks) || this.marks.length === 0) {
+		return 0;
+
+	}
+	const sum = this.marks.reduce((acc, mark) => acc + mark, 0);
+	return sum / this.marks.length;
+
 }
 
-Student.prototype.getAverage = function () {
+Student.prototype.exclude = function(reason) {
+	delete this.subject;
+	delete this.marks;
 
-  if (!Array.isArray(this.marks) || this.marks.length === 0) {
-  return 0;
-  
-  }
-  const sum = this.marks.reduce((acc, mark) => acc + mark, 0);
-  return sum / this.marks.length;
-
-}
-
-Student.prototype.exclude = function (reason) {
-  delete Student.subject    ;
-  delete Student.marks;
-
-  this.excluded = reason;
+	this.excluded = reason;
 }
 
 
